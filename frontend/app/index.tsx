c8f2e9ba122580c3b32e9dfd529f3d6a2c7bc746
Paarth Mishra <pmshot607@gmail.com>
2024-10-26 16:06:56 -0400

    camera + untested multipart code

---
+++ frontend/app/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={"/upload"}>Go to Camera</Link>
+      <Link href={"/details"}>Go to Details</Link>
+    </View>
+  );
+}
---
+++ frontend/app/upload.tsx
@@ -0,0 +1,121 @@
+import { CameraView, CameraProps, useCameraPermissions } from "expo-camera";
+import { useState, useEffect, useRef } from "react";
+import { Alert, Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Icon } from 'react-native-elements';
+
+export default function Upload() {
+  // @ts-ignore: just being lazy with types here
+  const cameraRef = useRef<CameraView>(undefined);
+  const [facing, setFacing] = useState<CameraProps["facing"]>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const [photoList, setPhotoList] = useState<Blob[]>([]);
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  async function takePhoto(){
+    const photo = await cameraRef.current?.takePictureAsync();
+    alert(`photo captured with dimensions: ${photo!.width} x ${photo!.height}`);
+    const uri: any = photo?.uri;
+    const resp = await fetch(uri);
+    const blob = await resp.blob();
+    setPhotoList([...photoList, blob]);
+    Alert.alert(JSON.stringify(blob));
+  }
+  async function sendImages() {
+    const url = "http://localhost:5000/image-receiver";
+    
+    try {
+      // Construct FormData to send the image as a multipart form
+      const formData = new FormData();
+      for(let i = 0;i<photoList.length;i++){
+        formData.append("file", photoList[i], "image"+i+".jpeg");
+      }
+      // Send the POST request with the image data
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: formData,
+      });
+  
+      if (response.ok) {
+        Alert.alert("Success", "Image uploaded successfully!");
+      } else {
+        Alert.alert("Error", "Failed to upload image.");
+      }
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      Alert.alert("Error", "An error occurred while uploading the image.");
+    }
+  }
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView
+        style={styles.camera}
+        facing={facing}
+        ref={cameraRef}
+      >
+      </CameraView>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={takePhoto}
+          style={{
+              borderWidth:1,
+              borderColor:'rgba(0,0,0,0.2)',
+              width:70,
+              height:70,
+              backgroundColor:'black',
+              borderRadius:50,
+            }}
+        >
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10
+  },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
